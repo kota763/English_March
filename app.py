63082334ae4f7a9e4cd01a84742ea54c2ff4ab8e
kota763 <kuroyagi5100@docomo.ne.jp>
2025-05-22 14:18:59 +0900

    gsdc

--- app.py
+++ app.py
@@ -760,7 +760,11 @@ def save_word():
     return redirect("/")
 
 
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
